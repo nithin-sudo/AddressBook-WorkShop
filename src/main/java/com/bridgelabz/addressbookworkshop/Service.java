70ac9c3efb91b045b4c6fe6a2bfd7937689295d6
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-02 01:24:33 +0530

    Added: Method and searching a person in a city.

--- src/main/java/com/bridgelabz/addressbookworkshop/Service.java
+++ src/main/java/com/bridgelabz/addressbookworkshop/Service.java
@@ -54,60 +54,78 @@ public class Service {
             System.out.println("New book created and added Contact Added Successfully");
         }
     }
-    public void editContact() {
-        String enteredFirstName;
-        System.out.println("Enter First name of contact to edit it ");
-        enteredFirstName = scanner.next();
-        for (int i = 0; i < personList.size(); i++) {
-            if (personList.get(i).getFirstName().equals(enteredFirstName)) {
-                System.out.println("Enter the field to edit:\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Phone\n8.Email");
-                int userInput = scanner.nextInt();
-                switch (userInput) {
-                    case 1:
-                        System.out.println("Enter new first name");
-                        personList.get(i).setFirstName(scanner.next());
-                        break;
-                    case 2:
-                        System.out.println("Enter new last name");
-                        personList.get(i).setLastName(scanner.next());
-                        break;
-                    case 3:
-                        System.out.println("Enter new Address");
-                        personList.get(i).setAddress(scanner.next());
-                        break;
-                    case 4:
-                        System.out.println("Enter new city");
-                        personList.get(i).setCity(scanner.next());
-                        break;
-                    case 5:
-                        System.out.println("Enter new state");
-                        personList.get(i).setState(scanner.next());
-                        break;
-                    case 6:
-                        System.out.println("Enter new zip");
-                        personList.get(i).setZip(scanner.next());
-                        break;
-                    case 7:
-                        System.out.println("Enter new phone number");
-                        personList.get(i).setPhoneNumber(scanner.next());
-                        break;
-                    case 8:
-                        System.out.println("Enter new email");
-                        personList.get(i).setEmail(scanner.next());
-                        break;
-                    default:
-                        System.out.println("Invalid Entry");
-
+    public void editContact(String bookName) {
+        if (addressBooks.containsKey(bookName)) {
+            String enteredFirstName;
+            System.out.println("Enter First name of contact to edit it ");
+            enteredFirstName = scanner.next();
+            for (int i = 0; i < personList.size(); i++) {
+                if (personList.get(i).getFirstName().equals(enteredFirstName)) {
+                    System.out.println("Enter the field to edit:\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Phone\n8.Email");
+                    int userInput = scanner.nextInt();
+                    switch (userInput) {
+                        case 1:
+                            System.out.println("Enter new first name");
+                            personList.get(i).setFirstName(scanner.next());
+                            break;
+                        case 2:
+                            System.out.println("Enter new last name");
+                            personList.get(i).setLastName(scanner.next());
+                            break;
+                        case 3:
+                            System.out.println("Enter new Address");
+                            personList.get(i).setAddress(scanner.next());
+                            break;
+                        case 4:
+                            System.out.println("Enter new city");
+                            personList.get(i).setCity(scanner.next());
+                            break;
+                        case 5:
+                            System.out.println("Enter new state");
+                            personList.get(i).setState(scanner.next());
+                            break;
+                        case 6:
+                            System.out.println("Enter new zip");
+                            personList.get(i).setZip(scanner.next());
+                            break;
+                        case 7:
+                            System.out.println("Enter new phone number");
+                            personList.get(i).setPhoneNumber(scanner.next());
+                            break;
+                        case 8:
+                            System.out.println("Enter new email");
+                            personList.get(i).setEmail(scanner.next());
+                            break;
+                        default:
+                            System.out.println("Invalid Entry");
+                    }
                 }
             }
         }
     }
-    public void deleteContact(String name) {
-        for (int i = 0; i < personList.size(); i++) {
-            if (personList.get(i).getFirstName().equals(name)) {
-                Person person = personList.get(i);
-                personList.remove(person);
+
+    /**
+     * deleting a person using person's name
+     *
+     * @param name
+     */
+    public void deleteContact(String name, String bookName) {
+        if (addressBooks.containsKey(bookName)) {
+            for (int i = 0; i < personList.size(); i++) {
+                if (personList.get(i).getFirstName().equals(name)) {
+                    Person person = personList.get(i);
+                    personList.remove(person);
+                }
             }
+        }
+    }
+    public void searchPersonInACity (String city)
+    {
+        System.out.println("following are the persons who belongs to :" + city);
+        for(String bookName : addressBooks.keySet())
+        {
+            addressBooks.get(bookName);
+            personList.stream().filter(value -> value.getCity().equals(city)).map(Person::getFirstName).forEach(System.out::println);
         }
     }
     public void displayList() {
