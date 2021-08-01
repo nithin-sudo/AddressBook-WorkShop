8deab5bea4d6118128feed91ee26995661b10666
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-02 00:23:17 +0530

    Added: initial setup for addressbooksystem.

---
+++ src/main/java/com/bridgelabz/addressbookworkshop/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookworkshop;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/bridgelabz/addressbookworkshop/Person.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.addressbookworkshop;
+
+public class Person {
+}
